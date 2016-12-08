ff18c138cf80a24859f76ecec1b9bd33162ad6b5
tran.hai.phong <tran.hai.phong@FRAMGIA.COM>
2016-12-08 08:59:04 +0700

    test

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,13 @@
 //=require jquery.elevatezoom.js
 //=require zoom.js
 //=require side-bar.js
+$(function(){
+	$('.target-menu').click(function(){
+		$('#menu-sub').css({'-webkit-transform' : 'translateX(0)',
+        'transform' : 'translateX(0)', 'visibility' : 'visible'});
+	});
+	$('.close').click(function(){
+		$('#menu-sub').css({'-webkit-transform' : 'translateX(-20em)',
+        'transform' : 'translateX(-20em)', 'visibility' : 'hidden'});
+	});
+});
